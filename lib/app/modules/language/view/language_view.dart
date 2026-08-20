55c83ab866f7607ae8541034b8791baa24fe3dae
Bunnareth7 <bunnareth.hor@gmail.com>
2026-08-20 15:21:54 +0700

    before change

--- lib/app/modules/language/view/language_view.dart
+++ lib/app/modules/language/view/language_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:kiosk_app/app/modules/login/views/login_view.dart';
-import 'package:kiosk_app/app/modules/start_order/view/start_order.dart';
 import 'package:kiosk_app/app/routes/app_pages.dart';
 
 class LanguageView extends StatelessWidget {
